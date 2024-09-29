435b0c61e76b09d2cc5dd48126579b95aa6ce88d
winrikls <winrikls@gmail.com>
2024-09-29 18:36:36 +0300

    Logical multiplication.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
 ﻿using System;
 
-namespace Program
+namespace Mini.Task_Sharp
 {
-    static void Main(string[] wsdctr)
+    public class Program
     {
-        
+        static void Main(string[] wsdctr)
+        {
+           Console.Write("Enter a: ");
+           int a = Convert.ToInt32(Console.ReadLine());
+
+           Console.Write("Enter b: ");
+           int b = Convert.ToInt32(Console.ReadLine());
+
+           int result = a & b;
+           Console.WriteLine("Result: " + result);
+
+           Console.ReadLine();
+        }
     }
 }
