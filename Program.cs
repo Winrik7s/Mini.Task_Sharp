42591c7a1195ba97c3fa1c12b9e8256cc2535319
winrikls <winrikls@gmail.com>
2025-04-06 17:14:07 +0300

    An example of finding the number pi by the Monte Carlo method.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,32 @@ namespace Mini.Task_Sharp
 {
     public class Program
     {
+        static double getPi(int n)
+        {
+            int i = 0;
+            int count = 0;
+
+            Random rnd = new Random();
+
+            while(i < n)
+            {
+                double x = rnd.NextDouble();
+                double y = rnd.NextDouble();
+
+                if(Math.Pow(x, 2) + Math.Pow(y,2) < 1)
+                {
+                    count++;
+                }
+
+                i++;
+            }
+
+            return 4 * ((double)count / n);
+        }
+
         static void Main(string[] wsdctr)
         {
-            
+            Console.WriteLine(getPi(1000000));
         }
     }
 }
