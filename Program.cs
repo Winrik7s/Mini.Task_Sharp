2568a7b55e0a52eb033163f0fb2ab2e9cd797c57
winrikls <winrikls@gmail.com>
2025-04-06 16:44:57 +0300

    Cleared the field.

--- Program.cs
+++ Program.cs
@@ -4,23 +4,9 @@ namespace Mini.Task_Sharp
 {
     public class Program
     {
-        static int Enc(ref int x)
-        {
-            int key = 24;
-            int enc = x ^ key;
-
-            return enc;
-        }
-
         static void Main(string[] wsdctr)
         {
-            Account account = new Account();
-
-            Console.Write("Enter the password to log in: ");
-            int pass = Convert.ToInt32(Console.ReadLine());
-
-            account.Password = Enc(ref pass);
-            Console.WriteLine(account.Password);
+            
         }
     }
 }
