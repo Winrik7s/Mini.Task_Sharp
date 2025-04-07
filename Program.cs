881a795889d91d965a82ce5fc35b8c838cbbbe3c
winrikls <winrikls@gmail.com>
2025-04-07 21:14:15 +0300

    Implementation of the Monte Carlo method in the form of hitting the target.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,32 @@ namespace Mini.Task_Sharp
 {
     public class Program
     {
+        static double Foo(int n)
+        {
+            int i = 0;
+            int c = 0;
+
+            Random rnd = new Random();
+
+            while(i < n)
+            {
+                double x = rnd.NextDouble();
+                double y = rnd.NextDouble();
+
+                if(Math.Pow(x,2) + Math.Pow(y,2) <= 1)
+                {
+                    c++;
+                }
+
+                i++;
+            }
+
+            return (double)c;
+        }
+
         static void Main(string[] wsdctr)
         {
-            
+            Console.WriteLine("Кол - во попаданий: " + Foo(10));
         }
     }
 }
